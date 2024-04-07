da57929ab7ebdc6445aec574bb62a613f416fd6d
ScorchingPotato <133220320+ScorchingPotato@users.noreply.github.com>
2024-04-07 18:28:03 +0300

    Update script.js
    Results are now working

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const questions = document.querySelectorAll('.question');
     const totalQuestions = questions.length;
     let currentQuestion = 0;
+    let totalscore = 0;
 
     function showQuestion(index) {
         questions.forEach((question, i) => {
@@ -23,24 +24,38 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     function showResults() {
-        // Placeholder content for results
-        const resultsContent = `
-            <p>Čia bus jūsų rezultatai...</p>
-        `;
+        const radioButtons = document.querySelectorAll('input[type="radio"]');
+        radioButtons.forEach((radioButton) => {
+            if (radioButton.checked) {
+                let selectedValue = radioButton.value;
+                totalscore+=Number(selectedValue);
+        };});
+        let response = "";
+        let response_color=0;
+        if (totalscore < 3) {
+            response = "Tikras klimato aktyvistas!"
+            response_color="#38ff66";
+        } else if (totalscore < 7) {
+            response = "Šaunuolis, tavo Co2 emisija yra mažesnės už vidutinio žmogaus"
+            response_color="#51f0d0"
+        } else if (totalscore < 8) {
+            response = "Tavo Co2 emisja yra tokia pat kaip vidutinio žmogaus"
+            response_color="#d5ff61"
+        } else {
+            response = "Tavo Co2 emisija yra labai didelė lyginant su vidutiniu žmogumi"
+            response_color="#ff5338"
+        }
     
-        // Display results in the modal
         const modal = document.getElementById('resultsModal');
         const resultsDiv = document.getElementById('resultsContent');
-        resultsDiv.innerHTML = resultsContent;
+        const resulttext= document.getElementById('results');
+        resulttext.innerHTML = response;
+        resulttext.style.color=response_color;
         modal.style.display = "block";
-    
-        // Close the modal when the close button is clicked
         const closeButton = document.getElementsByClassName("closeButton")[0];
         closeButton.onclick = function() {
             modal.style.display = "none";
         }
-    
-        // Close the modal when clicking outside of it
         window.onclick = function(event) {
             if (event.target == modal) {
                 modal.style.display = "none";
